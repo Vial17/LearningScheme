cb27b9af9d7a07286ba0286d0464d64a32f7bbfc
Vial17 <taowj17@163.com>
2015-07-01 18:02:17 +0800

    loop-display

--- loop-display.scm
+++ loop-display.scm
@@ -3,10 +3,10 @@
 
 (define loop-display
   (lambda(n s)
-    (if (= n 0)
-        null
+    (if (> n 0)
         (begin (display s)
                (newline)
-               (loop-display (- n 1) s)))))
+               (loop-display (- n 1) s))
+        null)))
 
 (loop-display 3 "Hello, world!")
